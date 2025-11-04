6e8a8206fa77bd7b2b0f6914c094ce4f1c8518d6
Neon <neon@saahild.com>
2025-11-03 23:42:32 -0500

    update deps

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
+app.use(express.json())
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Up`);
 });
+
