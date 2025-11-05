fc0af9c0f72ccbfc5a09f4a2225aabe90186d8d6
Neon <neon@saahild.com>
2025-11-04 23:45:15 -0500

    update: more code sob

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import { readFileSync } from "fs";
+import path from "path";
 
 const app = express();
 app.use(express.json());
+app.set("view engine", "ejs")
+app.set("views", path)
+
+app.get('/', (req, res) => {
+  res.render("index")
+})
+
+app.get("/mapdata", (req, res) => {
+  // ability to load partially or for x,y,zoom
+  const currentData = readFileSync("./data.json")
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Up`);
